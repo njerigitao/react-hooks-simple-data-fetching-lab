b5b240623be600f9062f81b81075985044b0ce05
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-29 19:33:54 +0300

    fetch callback

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,11 @@
 // create your App component here
+import React,{useState, useEffect} from "react";
+
+function App(){
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+
+}[])
+};
+
+export default App
