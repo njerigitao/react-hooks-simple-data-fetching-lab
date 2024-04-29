a0541e439c549ca2d847a15de81b4ce6edc68fa7
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-29 19:37:42 +0300

    set dog image url

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,13 @@
 import React,{useState, useEffect} from "react";
 
 function App(){
+    const[dogImage, setDogImage] = useState(null);
 useEffect(() => {
     fetch("https://dog.ceo/api/breeds/image/random")
+    .then(r => r.json())
+    .then(data => {
+        setDogImage(data.message);
+    })
 
 }[])
 };
